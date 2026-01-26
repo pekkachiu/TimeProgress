047e7211e29dec6d3044cb95cc765574eeb3446d
pekkachiu <seanpekka@gmail.com>
2026-01-26 16:32:10 +0800

    Initial Commit

---
+++ TimeProgress/TimeProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeProgressApp.swift
+//  TimeProgress
+//
+//  Created by PekkaChiu on 2026/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TimeProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

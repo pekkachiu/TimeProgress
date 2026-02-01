abcb00cd25cea0058a920ea1c2ed6e97aac5ae86
pekkachiu <seanpekka@gmail.com>
2026-02-01 18:05:25 +0800

    基礎功能能用

---
+++ widget new/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  widget new
+//
+//  Created by PekkaChiu on 2026/1/31.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ widget new/widget_newBundle.swift
@@ -0,0 +1,17 @@
+//
+//  widget_newBundle.swift
+//  widget new
+//
+//  Created by PekkaChiu on 2026/1/31.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widget_newBundle: WidgetBundle {
+    var body: some Widget {
+        widget_new()
+        widget_newControl()
+    }
+}
